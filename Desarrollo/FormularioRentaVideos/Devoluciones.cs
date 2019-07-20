56160949bdef325f3c890d6b160d61d88678fb46
aliaa115 <labrild@miumg.edu.gt>
2019-07-20 11:47:49 -0600

    Cambios varios
    Se cambio el nombre de Reportes a Devoluciones
    Se agregaron 4 paneles para las areas a tratar
    Se cambio los nombres de todos los elementos en el formulario

---
+++ Desarrollo/FormularioRentaVideos/Devoluciones.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FormularioRentaVideos
+{
+    public partial class Devoluciones : Form
+    {
+        public Devoluciones()
+        {
+            InitializeComponent();
+            Pnl_Nuevo.Visible = false;
+            Pnl_Modificar.Visible = false;
+            Pnl_Remover.Visible = false;
+            Pnl_Reporte.Visible = false;
+        }
+
+        private void Btn_Remover_Click(object sender, EventArgs e)
+        {
+            Pnl_Nuevo.Visible = false;
+            Pnl_Modificar.Visible = false;
+            Pnl_Remover.Visible = true;
+            Pnl_Remover.Dock = DockStyle.Fill;
+            Pnl_Reporte.Visible = false;
+        }
+
+        private void Btn_Modificar_Click(object sender, EventArgs e)
+        {
+            Pnl_Nuevo.Visible = false;
+            Pnl_Modificar.Visible = true;
+            Pnl_Modificar.Dock = DockStyle.Fill;
+            Pnl_Remover.Visible = false;
+            Pnl_Reporte.Visible = false;
+        }
+
+        private void Btn_Reporte_Click(object sender, EventArgs e)
+        {
+            Pnl_Nuevo.Visible = false;
+            Pnl_Modificar.Visible = false;
+            Pnl_Remover.Visible = true;
+            Pnl_Remover.Dock = DockStyle.Fill;
+            Pnl_Reporte.Visible = false;
+        }
+
+        private void Btn_Cerrar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void Btn_Minimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Btn_Nuevo_Click(object sender, EventArgs e)
+        {
+            Pnl_Nuevo.Visible = true;
+            Pnl_Nuevo.Dock = DockStyle.Fill;
+            Pnl_Modificar.Visible = false;
+            Pnl_Remover.Visible = false;
+            Pnl_Reporte.Visible = false;
+        }
+
+        private void Btn_Ayuda_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
