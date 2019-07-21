09401ad9924a5023190b9bc10eb20e85cec16435
Cristhian de Leon <cdeleonv3@miumg.edu.gt>
2019-07-21 12:12:42 -0600

    grd
    Guardo habitual.

---
+++ Desarrollo/FormularioRentaVideos/Renta/Frm_Renta.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace FormularioRentaVideos.Renta
+{
+    partial class Frm_Renta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Renta));
+            this.Pnl_Barra = new System.Windows.Forms.Panel();
+            this.Btn_ayuda = new System.Windows.Forms.Button();
+            this.Btn_minimzar = new System.Windows.Forms.Button();
+            this.Btn_Cerrar = new System.Windows.Forms.Button();
+            this.Pnl_Menu = new System.Windows.Forms.Panel();
+            this.Btn_modificar = new System.Windows.Forms.Button();
+            this.Btn_Reporte = new System.Windows.Forms.Button();
+            this.Btn_Remover = new System.Windows.Forms.Button();
+            this.Btn_Ingreso = new System.Windows.Forms.Button();
+            this.Pnl_mantenimiento = new System.Windows.Forms.Panel();
+            this.Pnl_Barra.SuspendLayout();
+            this.Pnl_Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Pnl_Barra
+            // 
+            this.Pnl_Barra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.Pnl_Barra.Controls.Add(this.Btn_ayuda);
+            this.Pnl_Barra.Controls.Add(this.Btn_minimzar);
+            this.Pnl_Barra.Controls.Add(this.Btn_Cerrar);
+            this.Pnl_Barra.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Pnl_Barra.Location = new System.Drawing.Point(0, 0);
+            this.Pnl_Barra.Name = "Pnl_Barra";
+            this.Pnl_Barra.Size = new System.Drawing.Size(675, 30);
+            this.Pnl_Barra.TabIndex = 0;
+            // 
+            // Btn_ayuda
+            // 
+            this.Btn_ayuda.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.Btn_ayuda.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Btn_ayuda.BackgroundImage")));
+            this.Btn_ayuda.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Btn_ayuda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_ayuda.Location = new System.Drawing.Point(561, 0);
+            this.Btn_ayuda.Name = "Btn_ayuda";
+            this.Btn_ayuda.Size = new System.Drawing.Size(30, 30);
+            this.Btn_ayuda.TabIndex = 2;
+            this.Btn_ayuda.UseVisualStyleBackColor = false;
+            // 
+            // Btn_minimzar
+            // 
+            this.Btn_minimzar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.Btn_minimzar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Btn_minimzar.BackgroundImage")));
+            this.Btn_minimzar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Btn_minimzar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_minimzar.Location = new System.Drawing.Point(597, 0);
+            this.Btn_minimzar.Name = "Btn_minimzar";
+            this.Btn_minimzar.Size = new System.Drawing.Size(30, 30);
+            this.Btn_minimzar.TabIndex = 1;
+            this.Btn_minimzar.UseVisualStyleBackColor = false;
+            // 
+            // Btn_Cerrar
+            // 
+            this.Btn_Cerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.Btn_Cerrar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Btn_Cerrar.BackgroundImage")));
+            this.Btn_Cerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Btn_Cerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Cerrar.Location = new System.Drawing.Point(633, 0);
+            this.Btn_Cerrar.Name = "Btn_Cerrar";
+            this.Btn_Cerrar.Size = new System.Drawing.Size(30, 30);
+            this.Btn_Cerrar.TabIndex = 0;
+            this.Btn_Cerrar.UseVisualStyleBackColor = false;
+            // 
+            // Pnl_Menu
+            // 
+            this.Pnl_Menu.BackColor = System.Drawing.Color.PeachPuff;
+            this.Pnl_Menu.Controls.Add(this.Btn_modificar);
+            this.Pnl_Menu.Controls.Add(this.Btn_Reporte);
+            this.Pnl_Menu.Controls.Add(this.Btn_Remover);
+            this.Pnl_Menu.Controls.Add(this.Btn_Ingreso);
+            this.Pnl_Menu.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Pnl_Menu.Location = new System.Drawing.Point(569, 30);
+            this.Pnl_Menu.Name = "Pnl_Menu";
+            this.Pnl_Menu.Size = new System.Drawing.Size(106, 370);
+            this.Pnl_Menu.TabIndex = 1;
+            // 
+            // Btn_modificar
+            // 
+            this.Btn_modificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_modificar.Location = new System.Drawing.Point(29, 199);
+            this.Btn_modificar.Name = "Btn_modificar";
+            this.Btn_modificar.Size = new System.Drawing.Size(50, 50);
+            this.Btn_modificar.TabIndex = 2;
+            this.Btn_modificar.Text = "button5";
+            this.Btn_modificar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Reporte
+            // 
+            this.Btn_Reporte.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Reporte.Location = new System.Drawing.Point(29, 279);
+            this.Btn_Reporte.Name = "Btn_Reporte";
+            this.Btn_Reporte.Size = new System.Drawing.Size(50, 50);
+            this.Btn_Reporte.TabIndex = 2;
+            this.Btn_Reporte.Text = "button5";
+            this.Btn_Reporte.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Remover
+            // 
+            this.Btn_Remover.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Remover.Location = new System.Drawing.Point(28, 116);
+            this.Btn_Remover.Name = "Btn_Remover";
+            this.Btn_Remover.Size = new System.Drawing.Size(50, 50);
+            this.Btn_Remover.TabIndex = 1;
+            this.Btn_Remover.Text = "button4";
+            this.Btn_Remover.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Ingreso
+            // 
+            this.Btn_Ingreso.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Ingreso.Location = new System.Drawing.Point(28, 41);
+            this.Btn_Ingreso.Name = "Btn_Ingreso";
+            this.Btn_Ingreso.Size = new System.Drawing.Size(50, 50);
+            this.Btn_Ingreso.TabIndex = 0;
+            this.Btn_Ingreso.Text = "button3";
+            this.Btn_Ingreso.UseVisualStyleBackColor = true;
+            // 
+            // Pnl_mantenimiento
+            // 
+            this.Pnl_mantenimiento.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Pnl_mantenimiento.Location = new System.Drawing.Point(0, 30);
+            this.Pnl_mantenimiento.Name = "Pnl_mantenimiento";
+            this.Pnl_mantenimiento.Size = new System.Drawing.Size(569, 370);
+            this.Pnl_mantenimiento.TabIndex = 2;
+            // 
+            // Frm_Renta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(675, 400);
+            this.Controls.Add(this.Pnl_mantenimiento);
+            this.Controls.Add(this.Pnl_Menu);
+            this.Controls.Add(this.Pnl_Barra);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Frm_Renta";
+            this.Text = "Frm_Renta";
+            this.Pnl_Barra.ResumeLayout(false);
+            this.Pnl_Menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Pnl_Barra;
+        private System.Windows.Forms.Button Btn_ayuda;
+        private System.Windows.Forms.Button Btn_minimzar;
+        private System.Windows.Forms.Button Btn_Cerrar;
+        private System.Windows.Forms.Panel Pnl_Menu;
+        private System.Windows.Forms.Button Btn_modificar;
+        private System.Windows.Forms.Button Btn_Reporte;
+        private System.Windows.Forms.Button Btn_Remover;
+        private System.Windows.Forms.Button Btn_Ingreso;
+        private System.Windows.Forms.Panel Pnl_mantenimiento;
+    }
+}
