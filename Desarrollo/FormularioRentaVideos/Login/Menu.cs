e428e2afb18f53c4594b6827462002b2f940efff
Cristhian de Leon <cdeleonv3@miumg.edu.gt>
2019-07-21 18:13:37 -0600

    Integracion modulo de seguridad.
    Merge del branch desa-mantenimiento-seguridad

--- Desarrollo/FormularioRentaVideos/Login/Menu.cs
+++ Desarrollo/FormularioRentaVideos/Login/Menu.cs
@@ -21,5 +21,10 @@ namespace FormularioRentaVideos.Login
         {
 
         }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
